cfd516ac8e85d6e2fed5a113d9d554657dbec578
albert shaymardanov <accretiondiskthat@gmail.com>
2022-09-05 15:44:35 +0500

    need commit for merge. And then need logic normal for user create

--- src/Examples02/Game.java
+++ src/Examples02/Game.java
@@ -18,8 +18,12 @@ public class Game {
         //#endregion
         while (candies > 0) {
             System.out.println("How many candies do you take?: ");
-            take = scanner.nextInt();
+
+            take = personTake(candies, max, min, scanner);
             candies -= take;
+            //take = scanner.nextInt();
+            candies -= take;
+
             botTake = hardBot(candies, max, min);
             candies -= botTake;
             System.out.printf("Bot take: %d left: %d candies", botTake, candies);
@@ -27,7 +31,14 @@ public class Game {
     }
 
     public int hardBot(int candies, int max, int min) {
-        int botTake = candies % (max + min);
-        return botTake;
+        return candies % (max + min);
+    }
+
+    public int personTake(int candies, int max, int min, Scanner scanner) {
+        int take;
+        do {
+            take = scanner.nextInt();
+        } while (take >= max || take >= candies || take <= min);
+            return take;
     }
 }
